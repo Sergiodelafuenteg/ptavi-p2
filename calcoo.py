a7a68cd2bc34e7b9fc5020231c64c72d78d75b10
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2017-09-25 21:16:21 +0200

    primera version terminada

--- calcoo.py
+++ calcoo.py
@@ -12,14 +12,17 @@ class Calculadora:
         self.value2 = value2
         #self.operador = operador
     def sum(self):
-
-         return self.value1 + self.value2
+        """add values"""
+        return self.value1 + self.value2
 
     def minus(self):
-
-         return self.value1 - self.value2
+        """substract values"""
+        return self.value1 - self.value2
 
 if __name__ == "__main__":
+
+    if len(sys.argv) != 4:
+        sys.exit("Usalo asi: python3 ca.py operando 1 operador operando 2")
 
     try:
         value1 = int(sys.argv[1])
@@ -29,9 +32,11 @@ if __name__ == "__main__":
     except ValueError:
         sys.exit("Error: Non numerical parameters")
 
-    print ("jajajaja")
     if operador == 'sum':
         resultado = calculadorabasic.sum()
     elif operador == 'rest':
         resultado = calculadorabasic.minus()
+    else:
+        sys.exit("Error: Only sum(add) or rest(substract)")
+
     print (resultado)
