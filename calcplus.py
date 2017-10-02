876825a823964955a3f6b2338a1f98a4f53971de
Sergio  De La Fuente Garcia <sfuente@gamma03.aulas.gsyc.urjc.es>
2017-10-02 16:34:30 +0200

    desarrollada suma

--- calcplus.py
+++ calcplus.py
@@ -3,17 +3,31 @@
 
 import sys
 import calcoohija
+import os
 
 file_calc = open(sys.argv[1], 'r')
 
-args = file_calc.readlines()
+lines = file_calc.readlines()
+
+#resultado = {}
 
 """evito saltos de linea"""
 
+for line in lines:
+    args = line.split(',')
+    operador = args[0]
+    resultado = 0
+    for arg in args[1:]:
+
+        if operador == 'suma':
+            resultado = calcoohija.CalculadoraHija(int(arg),resultado).suma()
+        if operador == 'resta':
+            resultado = calcoohija.CalculadoraHija(int(arg),resultado).resta()
+        if operador == 'multiplica':
+            resultado = calcoohija.CalculadoraHija(int(arg),resultado).multiplica()
+        if operador == 'divide':
+            resultado = calcoohija.CalculadoraHija(int(arg),resultado).divide()
+
 #args = args.remove('\n')
-print (args)
 
-for i in args:
-    i.rstrip(['\n'])
-
-print (args[0])
+print ('resultado = %s' % (resultado))
