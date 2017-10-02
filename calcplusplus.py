dd5c78275d507ee4031bae48f4fc1707ca18fee2
Sergio de la Fuente <Sergio@iMac-de-Sergio.local>
2017-10-02 20:24:36 +0200

    version con csv

--- calcplusplus.py
+++ calcplusplus.py
@@ -3,22 +3,22 @@
 
 import sys
 import calcoohija
+import csv
 
-file_calc = open(sys.argv[1], 'r')
+with open(sys.argv[1]) as file_calc:
 
-lines = file_calc.readlines()
+    lines = csv.reader(file_calc)
 
-for line in lines:
-    args = line.split(',')
-    operador = args[0]
-    resultado = int(args[1])
-    for arg in args[2:]:
-        if operador == 'suma':
-            resultado = calcoohija.CalculadoraHija(int(arg),resultado).suma()
-        if operador == 'resta':
-            resultado = calcoohija.CalculadoraHija(resultado,int(arg)).resta()
-        if operador == 'multiplica':
-            resultado = calcoohija.CalculadoraHija(int(arg),resultado).multiplica()
-        if operador == 'divide':
-            resultado = calcoohija.CalculadoraHija(resultado,int(arg)).divide()
-    print (resultado)
+    for line in lines:
+        operador = line[0]
+        resultado = int(line[1])
+        for arg in line[2:]:
+            if operador == 'suma':
+                resultado = calcoohija.CalculadoraHija(int(arg),resultado).suma()
+            if operador == 'resta':
+                resultado = calcoohija.CalculadoraHija(resultado,int(arg)).resta()
+            if operador == 'multiplica':
+                resultado = calcoohija.CalculadoraHija(int(arg),resultado).multiplica()
+            if operador == 'divide':
+                resultado = calcoohija.CalculadoraHija(resultado,int(arg)).divide()
+        print (resultado)
